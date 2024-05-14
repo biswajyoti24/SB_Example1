63ddcd825f6b240cf35868c463ef35a68f2365c8
PRASANTA KUMAR PARIDA <prasanta.parida@thinktalentindia.com>
2024-05-14 06:52:08 +0530

    issues fixed

--- src/main/java/com/example/sb_example1/business/impl/EmployeeDetailsServiceImpl.java
+++ src/main/java/com/example/sb_example1/business/impl/EmployeeDetailsServiceImpl.java
@@ -3,23 +3,27 @@ package com.example.sb_example1.business.impl;
 import com.example.sb_example1.business.services.EmployeeDetailsService;
 import com.example.sb_example1.model.EmployeeDetails;
 import com.example.sb_example1.repository.EmployeeDetailsRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeDetailsServiceImpl implements EmployeeDetailsService {
 
-  /*  @Autowired
-    private EmployeeDetailsRepository employeeDetailsRepository;*/
+    @Autowired
+    private EmployeeDetailsRepository employeeDetailsRepository;
+
     @Override
     public void insertEmployee(EmployeeDetails employee) {
+        // Creating a new EmployeeDetails object
+        EmployeeDetails emp = new EmployeeDetails();
 
-       /* EmployeeDetails emp = new EmployeeDetails();
-
+        // Setting values from the passed employee object
         emp.setName(employee.getName());
         emp.setEmail(employee.getEmail());
         emp.setStatus("Y");
-       employeeDetailsRepository.save(employee);*/
 
+        // Saving the new EmployeeDetails object
+        employeeDetailsRepository.save(emp);
     }
 }
